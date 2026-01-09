9d9376b42b3c420b8d2bb05c10d9a1fb578d6227
John Russell <johnrusselldln@gmail.com>
2026-01-09 14:16:13 +0800

    russ

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__ . '/hirams-backend/vendor/autoload.php';
+
+$app = require_once __DIR__ . '/hirams-backend/bootstrap/app.php';
+
+$app->handleRequest(
+    Illuminate\Http\Request::capture()
+);
+
